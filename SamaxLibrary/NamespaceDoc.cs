fb038472427b3296c4f5fba1104c964e2ceb5bc5
Sam <lpzampa@gmail.com>
2013-06-19 20:44:05 +0200

    Made an SHFB project for documentation

---
+++ SamaxLibrary/NamespaceDoc.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NamespaceDoc.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SamaxLibrary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Text;
+
+    /// <summary>
+    /// The <see cref="SamaxLibrary"/> namespace contains classes related to Battle.Net messages and packets.
+    /// </summary>
+    [CompilerGenerated]
+    [SuppressMessage(
+        "Microsoft.StyleCop.CSharp.MaintainabilityRules",
+        "SA1400:AccessModifierMustBeDeclared",
+        Justification = "CS1527: Elements defined in a namespace cannot be explicitly declared as private, protected, or protected internal")]
+    class NamespaceDoc
+    {
+    }
+}
---
+++ SamaxLibrary/SidMessages/NamespaceDoc.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NamespaceDoc.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SamaxLibrary.SidMessages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Text;
+
+    /// <summary>
+    /// The <see cref="SamaxLibrary.SidMessages"/> namespace contains classes related to the SID protocol of Battle.Net.
+    /// </summary>
+    [CompilerGenerated]
+    [SuppressMessage(
+        "Microsoft.StyleCop.CSharp.MaintainabilityRules",
+        "SA1400:AccessModifierMustBeDeclared",
+        Justification = "CS1527: Elements defined in a namespace cannot be explicitly declared as private, protected, or protected internal")]
+    class NamespaceDoc
+    {
+    }
+}
