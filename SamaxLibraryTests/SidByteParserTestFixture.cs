5e628f70823a6b3fed434cd0adffb9ae0c2d0a1a
Sam <lpzampa@gmail.com>
2013-07-23 22:23:31 +0200

    Wrote unit tests for SidByteParser.ReadInt32

--- SamaxLibraryTests/SidByteParserTestFixture.cs
+++ SamaxLibraryTests/SidByteParserTestFixture.cs
@@ -8,44 +8,91 @@
     public class SidByteParserTestFixture
     {
         [Test]
-        public void Constructor_WhenBytesNull_ThrowsArgumentNullException()
+        public void Constructor_WhenBytesIsNull_ThrowsArgumentNullException()
         {
-            AssertConstructorThrowsException<ArgumentNullException>(null);
+            AssertThatConstructorThrowsException<ArgumentNullException>(null);
         }
 
         [Test]
-        public void Constructor_WhenBytesEmpty_ThrowsArgumentException()
+        public void Constructor_WhenBytesIsEmpty_ThrowsArgumentException()
         {
             byte[] bytes = new byte[0];
-            AssertConstructorThrowsException<ArgumentException>(bytes);
+            AssertThatConstructorThrowsException<ArgumentException>(bytes);
         }
 
         [Test]
-        public void Constructor_WhenBytesJustTooSmall_ThrowsArgumentException()
+        public void Constructor_WhenBytesIsJustTooSmall_ThrowsArgumentException()
         {
             byte[] bytes = new byte[SidHeader.HeaderLength - 1];
-            AssertConstructorThrowsException<ArgumentException>(bytes);
+            AssertThatConstructorThrowsException<ArgumentException>(bytes);
         }
 
         [Test]
-        public void Constructor_WhenBytesJustLargeEnough_ParserHasNoDataBytesToParse()
+        public void Constructor_WhenBytesIsJustLargeEnough_ParserHasNoDataBytesToParse()
         {
             SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(0);
             Assert.That(parser.HasBytesToParse, Is.False);
         }
 
         [Test]
-        public void Constructor_WhenBytesJustLargeEnoughPlusOne_ParserHasDataBytesToParse()
+        public void Constructor_WhenBytesIsJustLargeEnoughPlusOne_ParserHasDataBytesToParse()
         {
             SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(1);
             Assert.That(parser.HasBytesToParse, Is.True);
         }
 
         [Test]
-        public void Constructor_WhenBytesVeryLarge_ParserHasDataBytesToParse()
+        public void Constructor_WhenBytesIsVeryLarge_ParserHasDataBytesToParse()
         {
             SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(100000);
             Assert.That(parser.HasBytesToParse, Is.True);
+        }
+
+        [Test]
+        public void ReadInt32_WhenNoDataBytes_ThrowsSidByteParserException()
+        {
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(0);
+            AssertThatReadInt32ThrowsException<SidByteParserException>(parser);
+        }
+
+        [Test]
+        public void ReadInt32_WhenJustTooFewDataBytes_ThrowsSidByteParserException()
+        {
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(3);
+            AssertThatReadInt32ThrowsException<SidByteParserException>(parser);
+        }
+
+        [Test]
+        public void ReadInt32_WhenJustEnoughDataBytes_ParserHasNoDataBytesToParse()
+        {
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(4);
+            parser.ReadInt32();
+            Assert.That(parser.HasBytesToParse, Is.False);
+        }
+
+        [Test]
+        public void ReadInt32_WhenJustEnoughDataBytesPlusOne_ParserHasDataBytesToParse()
+        {
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(5);
+            parser.ReadInt32();
+            Assert.That(parser.HasBytesToParse, Is.True);
+        }
+
+        //// TODO: Consider having a "WhenLotsOfDataBytes_ParserHasDataBytesToParse()
+
+        [TestCase(
+            1, 2, 3, 4,
+            ExpectedResult = 1 + 256 * (2 + 256 * (3 + 256 * 4)),
+            Description = "The integer should be read in little-endian.")]
+        [TestCase(
+            0, 0, 0, 0x80,
+            ExpectedResult = Int32.MinValue,
+            Description = "It should be possible to read the negative number of the greatest magnitude.")]
+        public Int32 ReadInt32(byte firstByte, byte secondByte, byte thirdByte, byte fourthByte)
+        {
+            byte[] dataBytes = { firstByte, secondByte, thirdByte, fourthByte };
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataBytes(dataBytes);
+            return parser.ReadInt32();
         }
 
         private SidByteParser CreateSidByteParser(byte[] bytesToParse)
@@ -59,9 +106,21 @@
             return CreateSidByteParser(bytes);
         }
 
-        private void AssertConstructorThrowsException<T>(byte[] bytes) where T : Exception
+        private SidByteParser CreateSidByteParserWithSpecifiedDataBytes(byte[] dataBytes)
         {
-            Assert.Throws<T>(() => CreateSidByteParser(bytes));
+            var bytes = new System.Collections.Generic.List<byte>() { 0, 0, 0, 0 };
+            bytes.AddRange(dataBytes);
+            return CreateSidByteParser(bytes.ToArray());
+        }
+
+        private void AssertThatConstructorThrowsException<T>(byte[] bytes) where T : Exception
+        {
+            Assert.That(() => CreateSidByteParser(bytes), Throws.InstanceOf<T>());
+        }
+
+        private void AssertThatReadInt32ThrowsException<T>(SidByteParser parser) where T : Exception
+        {
+            Assert.That(() => parser.ReadInt32(), Throws.InstanceOf<T>());
         }
     }
 }
