b36206c67695fbe194ef3458362382a593ffed40
Sam <lpzampa@gmail.com>
2013-07-24 01:07:19 +0200

    Wrote units tests for the ReadInt32AsEnum method
    Also did some refactoring of the old tests.
    
    Consider reducing the length of some of the method names. The longest
    method name comprises 114 characters!

---
+++ SamaxLibraryTests/SidByteParserTestEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace SamaxLibraryTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public enum SidByteParserTestEnum : int
+    {
+        Member0,
+        Member1,
+        EndiannessTestMember = 1 + 2 * 256 + 3 * 256 * 256 + 4 * 256 * 256 * 256
+    }
+}
--- SamaxLibraryTests/SidByteParserTestFixture.cs
+++ SamaxLibraryTests/SidByteParserTestFixture.cs
@@ -1,98 +1,169 @@
 ﻿namespace SamaxLibraryTests
 {
     using System;
+    using System.Collections.Generic;
     using NUnit.Framework;
+    using NUnit.Framework.Constraints;
     using SamaxLibrary.Sid;
 
     [TestFixture]
     public class SidByteParserTestFixture
     {
+        private const string OnlyTheFirstFourBytesShouldAffectTheResultDescription =
+            "Only the first four bytes should affect the result.";
+
+        private const string TheValueShouldBeReadInLittleEndianDescription =
+            "The value should be read in little-endian.";
+
         [Test]
         public void Constructor_WhenBytesIsNull_ThrowsArgumentNullException()
         {
-            AssertThatConstructorThrowsException<ArgumentNullException>(null);
+            AssertThat_Constructor_ThrowsException<ArgumentNullException>(null);
         }
 
         [Test]
         public void Constructor_WhenBytesIsEmpty_ThrowsArgumentException()
         {
             byte[] bytes = new byte[0];
-            AssertThatConstructorThrowsException<ArgumentException>(bytes);
+            AssertThat_Constructor_ThrowsException<ArgumentException>(bytes);
         }
 
         [Test]
         public void Constructor_WhenBytesIsJustTooSmall_ThrowsArgumentException()
         {
             byte[] bytes = new byte[SidHeader.HeaderLength - 1];
-            AssertThatConstructorThrowsException<ArgumentException>(bytes);
+            AssertThat_Constructor_ThrowsException<ArgumentException>(bytes);
         }
 
         [Test]
         public void Constructor_WhenBytesIsJustLargeEnough_ParserHasNoDataBytesToParse()
         {
-            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(0);
-            Assert.That(parser.HasBytesToParse, Is.False);
+            AssertThat_Constructor_WithSpecifiedDataByteCount_HasOrHasNotDataBytesToParse(0, false);
         }
 
         [Test]
         public void Constructor_WhenBytesIsJustLargeEnoughPlusOne_ParserHasDataBytesToParse()
         {
-            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(1);
-            Assert.That(parser.HasBytesToParse, Is.True);
+            AssertThat_Constructor_WithSpecifiedDataByteCount_HasOrHasNotDataBytesToParse(1, true);
         }
 
         [Test]
         public void Constructor_WhenBytesIsVeryLarge_ParserHasDataBytesToParse()
         {
-            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(100000);
-            Assert.That(parser.HasBytesToParse, Is.True);
+            AssertThat_Constructor_WithSpecifiedDataByteCount_HasOrHasNotDataBytesToParse(10000, true);
         }
 
         [Test]
         public void ReadInt32_WhenNoDataBytes_ThrowsSidByteParserException()
         {
-            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(0);
-            AssertThatReadInt32ThrowsException<SidByteParserException>(parser);
+            AssertThat_ReadInt32_WhenParserHasSpecifiedDataByteCount_ThrowsException<SidByteParserException>(0);
         }
 
         [Test]
         public void ReadInt32_WhenJustTooFewDataBytes_ThrowsSidByteParserException()
         {
-            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(3);
-            AssertThatReadInt32ThrowsException<SidByteParserException>(parser);
+            AssertThat_ReadInt32_WhenParserHasSpecifiedDataByteCount_ThrowsException<SidByteParserException>(3);
         }
 
         [Test]
         public void ReadInt32_WhenJustEnoughDataBytes_ParserHasNoDataBytesToParse()
         {
-            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(4);
-            parser.ReadInt32();
-            Assert.That(parser.HasBytesToParse, Is.False);
+            AssertThat_ReadInt32_WhenParserHasSpecifiedDataByteCount_HasOrHasNotDataBytesToParse(4, false);
         }
 
         [Test]
         public void ReadInt32_WhenJustEnoughDataBytesPlusOne_ParserHasDataBytesToParse()
         {
-            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(5);
-            parser.ReadInt32();
-            Assert.That(parser.HasBytesToParse, Is.True);
+            AssertThat_ReadInt32_WhenParserHasSpecifiedDataByteCount_HasOrHasNotDataBytesToParse(5, true);
         }
 
-        //// TODO: Consider having a "WhenLotsOfDataBytes_ParserHasDataBytesToParse()
+        [Test]
+        public void ReadInt32_WhenLotsOfDataBytes_ParserHasDataBytesToParse()
+        {
+            AssertThat_ReadInt32_WhenParserHasSpecifiedDataByteCount_HasOrHasNotDataBytesToParse(100000, true);
+        }
 
         [TestCase(
             1, 2, 3, 4,
             ExpectedResult = 1 + 256 * (2 + 256 * (3 + 256 * 4)),
-            Description = "The integer should be read in little-endian.")]
+            Description = TheValueShouldBeReadInLittleEndianDescription)]
         [TestCase(
             0, 0, 0, 0x80,
             ExpectedResult = Int32.MinValue,
             Description = "It should be possible to read the negative number of the greatest magnitude.")]
-        public Int32 ReadInt32(byte firstByte, byte secondByte, byte thirdByte, byte fourthByte)
+        [TestCase(
+            0, 0, 0, 0, (byte)1,
+            ExpectedResult = 0,
+            Description = OnlyTheFirstFourBytesShouldAffectTheResultDescription)]
+        public Int32 ReadInt32(
+            byte firstByte, byte secondByte, byte thirdByte, byte fourthByte, params Byte[] remainingBytes)
         {
-            byte[] dataBytes = { firstByte, secondByte, thirdByte, fourthByte };
-            SidByteParser parser = CreateSidByteParserWithSpecifiedDataBytes(dataBytes);
+            List<byte> dataBytes = new List<byte> { firstByte, secondByte, thirdByte, fourthByte };
+            dataBytes.AddRange(remainingBytes);
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataBytes(dataBytes.ToArray());
             return parser.ReadInt32();
+        }
+
+        [Test]
+        public void ReadInt32AsEnum_WhenTypeParameterIsNotAnEnumerationType_ThrowsArgumentException()
+        {
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(4);
+            Assert.That(() => parser.ReadInt32AsEnum<Int64>(), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void ReadInt32AsEnum_WhenNoDataBytes_ThrowsSidByteParserException()
+        {
+            AssertThat_ReadInt32AsEnum_WhenEnumTypeIsTestEnumAndParserHasSpecifiedDataByteCount_ThrowsException<SidByteParserException>(0);
+        }
+
+        [Test]
+        public void ReadInt32AsEnum_WhenJustTooFewDataBytes_ThrowsSidByteParserException()
+        {
+            AssertThat_ReadInt32AsEnum_WhenEnumTypeIsTestEnumAndParserHasSpecifiedDataByteCount_ThrowsException<SidByteParserException>(3);
+        }
+
+        [Test]
+        public void ReadInt32AsEnum_WhenReadValueDoesNotMatchAnyEnumMember_ThrowsSidByteParserException()
+        {
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataBytes(2, 0, 0, 0);
+            Assert.That(
+                () => parser.ReadInt32AsEnum<SidByteParserTestEnum>(),
+                Throws.InstanceOf<SidByteParserException>());
+        }
+
+        [Test]
+        public void ReadInt32AsEnum_WhenJustEnoughDataBytes_ParserHasNoDataBytesToParse()
+        {
+            AssertThat_ReadInt32AsEnum_WhenEnumTypeIsTestEnumAndParserHasSpecifiedDataByteCount_HasOrHasNotDataBytesToParse(4, false);
+        }
+
+        [Test]
+        public void ReadInt32AsEnum_WhenJustEnoughDataBytesPlusOne_ParserHasDataBytesToParse()
+        {
+            AssertThat_ReadInt32AsEnum_WhenEnumTypeIsTestEnumAndParserHasSpecifiedDataByteCount_HasOrHasNotDataBytesToParse(5, true);
+        }
+
+        public void ReadInt32AsEnum_WhenLotsOfDataBytes_ParserHasDataBytesToParse()
+        {
+            AssertThat_ReadInt32AsEnum_WhenEnumTypeIsTestEnumAndParserHasSpecifiedDataByteCount_HasOrHasNotDataBytesToParse(4, false);
+        }
+
+        [TestCase(
+            1, 2, 3, 4,
+            ExpectedResult = SidByteParserTestEnum.EndiannessTestMember,
+            Description = TheValueShouldBeReadInLittleEndianDescription)]
+        [TestCase(
+            0, 0, 0, 0, (byte)1,
+            ExpectedResult = SidByteParserTestEnum.Member0,
+            Description = OnlyTheFirstFourBytesShouldAffectTheResultDescription)]
+        public SidByteParserTestEnum ReadInt32AsEnum_WhenEnumTypeIsTestEnum(
+            byte firstByte, byte secondByte, byte thirdByte, byte fourthByte, params byte[] remainingBytes)
+        {
+            List<byte> dataBytes = new List<byte> { firstByte, secondByte, thirdByte, fourthByte };
+            dataBytes.AddRange(remainingBytes);
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataBytes(dataBytes.ToArray());
+            return parser.ReadInt32AsEnum<SidByteParserTestEnum>();
         }
 
         private SidByteParser CreateSidByteParser(byte[] bytesToParse)
@@ -106,21 +177,79 @@
             return CreateSidByteParser(bytes);
         }
 
-        private SidByteParser CreateSidByteParserWithSpecifiedDataBytes(byte[] dataBytes)
+        private SidByteParser CreateSidByteParserWithSpecifiedDataBytes(params byte[] dataBytes)
         {
             var bytes = new System.Collections.Generic.List<byte>() { 0, 0, 0, 0 };
             bytes.AddRange(dataBytes);
             return CreateSidByteParser(bytes.ToArray());
         }
 
-        private void AssertThatConstructorThrowsException<T>(byte[] bytes) where T : Exception
+        private void AssertThat_Constructor_ThrowsException<T>(byte[] bytes) where T : Exception
         {
             Assert.That(() => CreateSidByteParser(bytes), Throws.InstanceOf<T>());
         }
 
-        private void AssertThatReadInt32ThrowsException<T>(SidByteParser parser) where T : Exception
+        private void AssertThat_Constructor_WithSpecifiedDataByteCount_HasOrHasNotDataBytesToParse(
+            int dataByteCount, bool shouldHaveDataBytesToParse)
+        {
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(dataByteCount);
+            IResolveConstraint constraint = shouldHaveDataBytesToParse ? (IResolveConstraint)Is.True : Is.False;
+            Assert.That(parser.HasBytesToParse, constraint);
+        }
+
+        private void AssertThat_ReadInt32_ThrowsException<T>(SidByteParser parser) where T : Exception
         {
             Assert.That(() => parser.ReadInt32(), Throws.InstanceOf<T>());
+        }
+
+        private void AssertThat_ReadInt32_WhenParserHasSpecifiedDataByteCount_ThrowsException<T>(
+            int dataByteCount)
+            where T : Exception
+        {
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(dataByteCount);
+            AssertThat_ReadInt32_ThrowsException<T>(parser);
+        }
+
+        private void AssertThat_ReadInt32_WhenParserHasSpecifiedDataByteCount_HasOrHasNotDataBytesToParse(
+            int dataByteCount, bool shouldHaveDataBytesToParse)
+        {
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(dataByteCount);
+            parser.ReadInt32();
+            IResolveConstraint constraint = shouldHaveDataBytesToParse ? (IResolveConstraint)Is.True : Is.False;
+            Assert.That(parser.HasBytesToParse, constraint);
+        }
+
+        private void AssertThat_ReadInt32AsEnum_ThrowsException<TEnum, TException>(SidByteParser parser)
+            where TEnum : struct, IComparable, IConvertible, IFormattable
+            where TException : Exception
+        {
+            Assert.That(() => parser.ReadInt32AsEnum<TEnum>(), Throws.InstanceOf<TException>());
+        }
+
+        private void AssertThat_ReadInt32AsEnum_WhenEnumTypeIsTestEnum_ThrowsException<T>(
+            SidByteParser parser)
+            where T : Exception
+        {
+            AssertThat_ReadInt32AsEnum_ThrowsException<SidByteParserTestEnum, T>(parser);
+        }
+
+        private void AssertThat_ReadInt32AsEnum_WhenEnumTypeIsTestEnumAndParserHasSpecifiedDataByteCount_ThrowsException<T>(
+            int dataByteCount)
+            where T : Exception
+        {
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataByteCount(dataByteCount);
+            AssertThat_ReadInt32AsEnum_WhenEnumTypeIsTestEnum_ThrowsException<T>(parser);
+        }
+
+        private void AssertThat_ReadInt32AsEnum_WhenEnumTypeIsTestEnumAndParserHasSpecifiedDataByteCount_HasOrHasNotDataBytesToParse(
+            int dataByteCount, bool shouldHaveDataBytesToParse)
+        {
+            // Explicitness about the data bytes to make sure that there is an enum member matching the read value
+            byte[] dataBytes = new byte[dataByteCount];
+            SidByteParser parser = CreateSidByteParserWithSpecifiedDataBytes(dataBytes);
+            parser.ReadInt32AsEnum<SidByteParserTestEnum>();
+            IResolveConstraint constraint = shouldHaveDataBytesToParse ? (IResolveConstraint)Is.True : Is.False;
+            Assert.That(parser.HasBytesToParse, constraint);
         }
     }
 }
