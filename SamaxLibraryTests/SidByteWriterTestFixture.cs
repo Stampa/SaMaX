3035297448eb9070243301a9fc865e1c4f5535da
Sam <lpzampa@gmail.com>
2013-08-03 21:06:33 +0200

    Started working on unit tests for SidByteWriter

---
+++ SamaxLibraryTests/SidByteWriterTestFixture.cs
@@ -0,0 +1,22 @@
+﻿namespace SamaxLibraryTests
+{
+    using System;
+    using NUnit.Framework;
+    using SamaxLibrary.Sid;
+
+    [TestFixture]
+    public class SidByteWriterTestFixture
+    {
+        [Test]
+        public void Constructor_BytesIsEmpty()
+        {
+            SidByteWriter writer = CreateSidByteWriter();
+            Assert.That(writer.Bytes.Length, Is.EqualTo(0));
+        }
+
+        private SidByteWriter CreateSidByteWriter()
+        {
+            return new SidByteWriter();
+        }
+    }
+}
