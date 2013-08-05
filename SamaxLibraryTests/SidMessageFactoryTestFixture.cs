5df4af40f0f39789acc6bc69cde7c31865a0d121
Sam <lpzampa@gmail.com>
2013-08-05 02:50:10 +0200

    Started working on unit tests for the factory
    The factory for SID messages to be specific.

---
+++ SamaxLibraryTests/SidMessageFactoryTestFixture.cs
@@ -0,0 +1,29 @@
+﻿namespace SamaxLibraryTests
+{
+    using System;
+    using NUnit.Framework;
+    using SamaxLibrary.Sid;
+
+    [TestFixture]
+    public class SidMessageFactoryTestFixture
+    {
+        private const byte InvalidSidMessageType = 0xFF;
+
+        [Test]
+        public void CreateClientToServerMessageFromBytes_WhenMessageBytesIsNull_ThrowsArgmuentNullException()
+        {
+            Assert.That(
+                () => SidMessageFactory.CreateClientToServerMessageFromBytes(null),
+                Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void CreateClientToServerMessageFromBytes_WhenMessageTypeIsInvalid_ThrowsArgumentException()
+        {
+            byte[] messageBytes = new byte[4] { 0xFF, InvalidSidMessageType, 0, 0 };
+            Assert.That(
+                () => SidMessageFactory.CreateClientToServerMessageFromBytes(messageBytes),
+                Throws.ArgumentException);
+        }
+    }
+}
