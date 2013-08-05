e94a65c71cd0653660a56416770fd698962b1eff
Sam <lpzampa@gmail.com>
2013-08-05 17:32:10 +0200

    Wrote a C>S SID_PING class with some unit tests
    The two SID_PING classes are essentially the same (find and replace client
    with server and vice versa). This might be okay, but the boilerplate code
    shared by all SID messages should probably be reduced at the expense of
    simplicity.
    
    There will probably be similar problems with the corresponding unit test
    fixtures.

--- SamaxLibrary/Sid/PingClientToServerSidMessage.cs
+++ SamaxLibrary/Sid/PingClientToServerSidMessage.cs
@@ -7,32 +7,32 @@
     using System.Threading.Tasks;
 
     /// <summary>
-    /// This class represents a SID_PING message sent from the server to the client.
+    /// This class represents a SID_PING message sent from the client to the server.
+    /// It is sent in response to a SID_PING message sent from the server.
     /// </summary>
-    public class PingServerToClientSidMessage : SidMessage
+    public class PingClientToServerSidMessage : SidMessage
     {
         /// <summary>
         /// The SID message type of the SID message that the
-        /// <see cref="PingServerToClientSidMessage"/> class represents.
+        /// <see cref="PingClientToServerSidMessage"/> class represents.
         /// </summary>
         public new const SidMessageType MessageType = SidMessageType.Ping;
 
         /// <summary>
-        /// Gets the ping value, which the client is supposed to immediately return in a
-        /// client-to-server ping message.
+        /// Gets the ping value, which is obtained from the server-to-client ping message.
         /// </summary>
-        /// <seealso cref="PingClientToServerSidMessage.PingValue"/>
+        /// <seealso cref="PingServerToClientSidMessage.PingValue"/>
         public Int32 PingValue { get; private set; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PingServerToClientSidMessage"/> class.
+        /// Initializes a new instance of the <see cref="PingClientToServerSidMessage"/> class.
         /// </summary>
         /// <param name="messageBytes">An array of bytes that composes the message.</param>
         /// <exception cref="ArgumentNullException"><paramref name="messageBytes"/> is <c>null</c>.
         /// </exception>
         /// <exception cref="ArgumentException"><paramref name="messageBytes"/> does not represent
         /// a valid server-to-client SID_PING message.</exception>
-        public PingServerToClientSidMessage(byte[] messageBytes)
+        public PingClientToServerSidMessage(byte[] messageBytes)
             : base(messageBytes, MessageType)
         {
             if (messageBytes == null)
